5d6e4457791508fabb463035b930c8c3c096e4e0
tragedyhk <alex99hk@gmail.com>
2020-12-25 14:50:57 +0800

    Added thread

--- control-flow/src/main.rs
+++ control-flow/src/main.rs
@@ -2,14 +2,14 @@ use std::time::Instant;
 
 mod if_condition;
 mod loops;
+mod thread;
 
-
-
-fn main() {
+pub fn main() {
     let start = Instant::now();
     //if_condition::print_if();
     //loops::print_loop();
-
+    thread::print_thread();
     let duration = start.elapsed();
     println!("Time elapsed is: {:?}", duration);
+
 }
---
+++ control-flow/src/thread.rs
@@ -0,0 +1,47 @@
+use std::time::Instant;
+use std::thread::{spawn,JoinHandle};
+
+fn fibonacci(input: u64) -> u64 {
+    let mut a: u64 = 0;
+    let mut b: u64 = 1;
+    for _ in 0..input {
+        let t = a + b;
+        a = b;
+        b = t;
+    }
+    return b;
+}
+
+fn fibonacci_thread(input: u64) -> JoinHandle<u64>{
+    let t = spawn(move || {
+        return fibonacci(input);
+    });
+    return t;
+}
+
+pub fn print_thread() {
+    let start_st = Instant::now();
+    fibonacci(300);
+    fibonacci(300);
+    fibonacci(300);
+    fibonacci(300);
+    fibonacci(300);
+    let duration_st = start_st.elapsed();
+    println!("Single Thread is: {:?}", duration_st);
+
+
+    let start_mt = Instant::now();
+    let t1 = fibonacci_thread(300);
+    let t2 = fibonacci_thread(300);
+    let t3 = fibonacci_thread(300);
+    let t4 = fibonacci_thread(300);
+    let t5 = fibonacci_thread(300);
+
+    t1.join().unwrap();
+    t2.join().unwrap();
+    t3.join().unwrap();
+    t4.join().unwrap();
+    t5.join().unwrap();
+    let duration_mt = start_mt.elapsed();
+    println!("Multi Thread is: {:?}", duration_mt);
+}
